73946430419de7440bbb4a4f49a9650bebb67e7e
Darya Sushkova <dasha.030105@gmail.com>
2025-06-14 19:04:52 +0300

    refactor: replace app to components folder and change styles

---
+++ src/app/app.jsx
@@ -0,0 +1,19 @@
+import AppHeader from "../components/app-header";
+import BurgerIngredients from "../components/burger-ingredients";
+import { data } from "../utils/data";
+import styles from "./app.module.css";
+
+function App() {
+  const appData = data;
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients ingredients={appData} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
