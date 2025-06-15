d48770ceea2bf994b8a836e79ab5cfc99960eab9
Darya Sushkova <dasha.030105@gmail.com>
2025-06-16 00:14:27 +0300

    chore: add ingredients fetch api instead of a hardcode

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,31 +1,75 @@
 import AppHeader from "../app-header";
 import BurgerIngredients from "../burger-ingredients";
-import { data } from "../../utils/data";
 import styles from "./app.module.css";
 import BurgerConstructor from "../burger-constructor";
+import { apiIngredients } from "../../utils/api";
+import { useEffect, useState } from "react";
+import { RingLoader } from "react-spinners";
 
 function App() {
-  const appData = data;
+  const [state, setState] = useState({
+    ingredients: [],
+    loading: true,
+    hasError: false,
+  });
+
+  useEffect(() => {
+    const getIngredientsData = async () => {
+      fetch(apiIngredients)
+        .then((res) => res.json())
+        .then((data) =>
+          setTimeout(() => {
+            setState((prev) => ({
+              ...prev,
+              ingredients: data.data,
+              loading: false,
+            }));
+          }, 1000)
+        )
+        .catch((_e) =>
+          setState((prev) => ({ ...prev, loading: false, hasError: true }))
+        );
+    };
+
+    getIngredientsData();
+  }, []);
 
   return (
     <div className={styles.app}>
       <AppHeader />
-      <main className={styles.main}>
-        <BurgerIngredients ingredients={appData} />
-        <BurgerConstructor
-          ingredients={appData}
-          mainBunId={appData[0]._id}
-          selectedIngredientsIds={[
-            "60666c42cc7b410027a1a9b9",
-            "60666c42cc7b410027a1a9b4",
-            "60666c42cc7b410027a1a9bc",
-            "60666c42cc7b410027a1a9bb",
-            "60666c42cc7b410027a1a9bb",
-            "60666c42cc7b410027a1a9bd",
-            "60666c42cc7b410027a1a9bf",
-          ]}
-        />
-      </main>
+      {(state.loading || state.hasError) && (
+        <div className={styles.state}>
+          {state.loading ? (
+            <RingLoader
+              color="var(--dark-grey)"
+              loading={state.loading}
+              size={100}
+            />
+          ) : (
+            <p className="text text_type_main-default text_color_inactive">
+              Произошла ошибка при получении игредиентов
+            </p>
+          )}
+        </div>
+      )}
+      {!state.loading && !state.hasError && state.ingredients.length > 0 && (
+        <main className={styles.main}>
+          <BurgerIngredients ingredients={state.ingredients} />
+          <BurgerConstructor
+            ingredients={state.ingredients}
+            mainBunId={state.ingredients[0]._id}
+            selectedIngredientsIds={[
+              "643d69a5c3f7b9001cfa093c",
+              "643d69a5c3f7b9001cfa0941",
+              "643d69a5c3f7b9001cfa093e",
+              "643d69a5c3f7b9001cfa0942",
+              "643d69a5c3f7b9001cfa0942",
+              "643d69a5c3f7b9001cfa0943",
+              "643d69a5c3f7b9001cfa093f",
+            ]}
+          />
+        </main>
+      )}
     </div>
   );
 }
