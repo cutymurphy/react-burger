799353533f4c67d7de6ab4017ee963c5c73cd3a6
Darya Sushkova <dasha.030105@gmail.com>
2025-07-12 18:46:06 +0300

    style: some app style fixes

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -14,7 +14,10 @@ function AppHeader() {
     <header className={styles.header}>
       <nav className={`${styles.header__content} pt-4 pb-4`}>
         <ul className={styles.header__nav}>
-          <li className={`${styles.header__btn} pt-4 pr-5 pb-4 pl-5 mr-2`}>
+          <li
+            className={`${styles.header__btn} pt-4 pr-5 pb-4 pl-5 mr-2`}
+            onClick={() => navigate("/")}
+          >
             <BurgerIcon type="primary" className="mr-2" />
             <p className="text text_type_main-default">Конструктор</p>
           </li>
@@ -32,12 +35,11 @@ function AppHeader() {
         </div>
         <ul className={styles.header__nav}>
           <li
-            className={`${styles.header__btn} ${styles.disabled} pt-4 pr-5 pb-4 pl-5`}
+            className={`${styles.header__btn} pt-4 pr-5 pb-4 pl-5`}
+            onClick={() => navigate("/profile")}
           >
-            <ProfileIcon type="secondary" className="mr-2" />
-            <p className="text text_type_main-default text_color_inactive">
-              Личный кабинет
-            </p>
+            <ProfileIcon type="primary" className="mr-2" />
+            <p className="text text_type_main-default">Личный кабинет</p>
           </li>
         </ul>
       </nav>
